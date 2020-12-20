b48947c918674d6f98af60d093a2149d660392ae
ll_casper_ll <you@example.com>
2020-12-21 00:46:20 +0200

    .

--- src/pages/Fractals/Fractals.jsx
+++ src/pages/Fractals/Fractals.jsx
@@ -1,37 +1,39 @@
-import { MainWrapper } from "layouts/MainWrapper";
-import React, { useEffect, useState } from "react";
+// import { MainWrapper } from "layouts/MainWrapper";
+// import React, { useEffect, useState } from "react";
 
-export const Fractals = () => {
-  const [sliderValue, setSliderValue] = useState(0);
-  useEffect(() => {
-    console.log(document.body.classList);
-    document.body.style.backgroundColor = "white";
-  }, []);
+// export const Fractals = () => {
+//   const [sliderValue, setSliderValue] = useState(0);
+//   useEffect(() => {
+//     console.log(document.body.classList);
+//     document.body.style.backgroundColor = "white";
+//   }, []);
 
-  return (
-    <div>
-      <MainWrapper>
-        <div>
-          <input
-            type="range"
-            min={1}
-            max={20}
-            value={sliderValue}
-            onChange={(e) => {
-              // @ts-ignore
-              setSliderValue(e.target.value);
-            }}
-            className="slider"
-            id="myRange"
-          />
-          <p>
-            Value: <span id="demo"></span>
-          </p>
-        </div>
-      </MainWrapper>
-      <div>
-        <canvas id="c" height="600" width="840"></canvas>
-      </div>
-    </div>
-  );
-};
+//   return (
+//     <div>
+//       <MainWrapper
+// // @ts-ignore
+//       MainWrapper>
+//         <div>
+//           <input
+//             type="range"
+//             min={1}
+//             max={20}
+//             value={sliderValue}
+//             onChange={(e) => {
+//               // @ts-ignore
+//               setSliderValue(e.target.value);
+//             }}
+//             className="slider"
+//             id="myRange"
+//           />
+//           <p>
+//             Value: <span id="demo"></span>
+//           </p>
+//         </div>
+//       </MainWrapper>
+//       <div>
+//         <canvas id="c" height="600" width="840"></canvas>
+//       </div>
+//     </div>
+//   );
+// };
